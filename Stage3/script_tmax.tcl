fd784678957b4a65138d130fd50bd3aa7ade4799
Nikolas Koxenoglou <koxakin@gmail.com>
2018-04-21 18:50:37 +0300

    Added more detaild analisys tcl commands to tmax tcl script

--- Stage3/script_tmax.tcl
+++ Stage3/script_tmax.tcl
@@ -1,6 +1,8 @@
 set_scan_configuration
 create_test_protocol -infer_asynch -infer_clock
-dft_drc
+set_dft_configuration -fix_reset enable -fix_clock enable 
+dft_drc -coverage_estimate
+preview_dft
 insert_dft
 write_test_protocol -output simd_stil.stil
 write -h -f verilog -o simd_netlist_scan.v
