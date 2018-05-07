b2a45613ef05c0bc26ac724a8e4830fadb3d0913
Nikolas Koxenoglou <koxakin@gmail.com>
2018-05-07 16:53:18 +0300

    Updated min script to reflect the Sotos one compile at max flow and extract the sdc and sdf files from the min libraries as well as the hold timing analysis

--- Stage3/script_min.tcl
+++ Stage3/script_min.tcl
@@ -1,13 +1,15 @@
-analyze -library WORK -format verilog { ./simd_top_level.v ./alu.v ./single_port_ram.v ./core_control.v ./mem_ctrl.v }
-elaborate simd_top_level -architecture verilog -library WORK
-set_design_top simd_top_level
+#analyze -library WORK -format verilog { ./simd_top_level.v ./alu.v ./single_port_ram.v ./core_control.v ./mem_ctrl.v }
+#elaborate simd_top_level -architecture verilog -library WORK
+read_verilog -netlist ./simd_netlist.v
+current_design simd_top_level
+#set_design_top simd_top_level
 create_clock "clk" -period 80
 create_clock "clk_2" -period 20
 set_drive 1.08263 [all_inputs]
 set_load 0.006006 [all_outputs]
-set_false_path -from reset 
-uniquify
-compile -no_design_rule
+#set_false_path -from reset 
+#uniquify
+#compile -no_design_rule
 report_timing -delay_type min
 write_sdf simd_sdf_min.sdf
 write_sdc simd_sdc_min.sdc
